85e8245f20646a7e3162a852f6e09ba0815e6394
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-11-04 17:41:24 -0600

    Update module.js

--- module.js
+++ module.js
@@ -1,24 +1,20 @@
 var elevador = (function () {
     let pisoUsuario=0;
-    let tuPiso=0;
+    //let tuPiso=0;
     let pisoActual=0;
     let edoPuerta=false; // cerrada=false abierta=true
     let cantPisos=5;
 
-    this.irPisoUsuario=function(tuPiso){//elevador se mueve de pisoActual a pisoUsuario
+    /*this.irPisoUsuario=function(tuPiso){//elevador se mueve de pisoActual a pisoUsuario
         if(pisoActual>tuPiso){
             this.bajar();
         }
         else{
             this.subir();
         }
-    }
-    this.irA=function(pisoDestino,tuPiso){//elevador se mueve de pisoUsuario a pisoDestino
-        if(tuPiso<0 || tuPiso>5){
-            console.log('piso actual inexistente')
-        }
-        else if(pisoDestino>=0 && pisoDestino<=5){
-            irPisoUsuario(tuPiso);
+    }*/
+    this.irA=function(pisoDestino){//elevador se mueve de pisoUsuario a pisoDestino
+            //irPisoUsuario(tuPiso);
             while(tuPiso>pisoDestino){
                 console.log(`piso ${bajar()}`);
             }
@@ -26,21 +22,20 @@ var elevador = (function () {
                 console.log(`piso ${subir()}`);
             }
             pisoUsuario=pisoDestino;
-            pisoActual=pisoDestino;
-        }    
+            pisoActual=pisoDestino;    
     }
     this.subir=function(){
         cerrarPuerta();
         if(pisoActual<cantPisos){
             // console.log(`piso ${pisoActual++}`);
-            pisoActual++;
+            return pisoActual++;
         }
     }
     this.bajar=function(){
         cerrarPuerta();
         if(pisoActual>0){
             // console.log(`piso${pisoActual--}`);
-            pisoActual--;
+            return pisoActual--;
         }        
     }
     this.abrirPuerta=function(){
@@ -52,7 +47,7 @@ var elevador = (function () {
             edoPuerta=false;
     }
     this.estado=function(){ // cuando intento imprimirlo en consola, solo me arroja que es una función
-        console.log(`elevador en piso ${pisoActual} de 5, puerta ${edoPuerta}`);
+        return(`elevador en piso ${pisoActual} de 5, puerta ${edoPuerta}`);
     }
     return {
         abrirPuerta:abrirPuerta,
